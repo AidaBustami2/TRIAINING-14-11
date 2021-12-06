26404b79f196d389d7bd81a03f30059853ff5f7c
Hp <aida_bustami@yahoo.com>
2021-12-06 15:34:32 +0200

    THREAD   6/12

---
+++ demo/src/main/java/com/example/demo/test/Models/Thread.java
@@ -0,0 +1,19 @@
+package com.example.demo.test.Models;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+@Getter
+@Setter
+@Entity
+public class Thread {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "thread_id", nullable = false)
+    Long thread_id;
+    boolean busy = false;
+    @OneToOne(mappedBy = "thread",  fetch = FetchType.EAGER)
+    Task task;
+    public Thread() {
+    }
+}
